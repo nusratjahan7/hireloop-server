f9202d5c60db3bde6eed96a8cbbd84a41f23e9d1
nusratjahan7 <nusratjahan77222@gmail.com>
2026-06-11 09:28:57 +0600

    Hireloop Backend setup and creating the server

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+const cors = require('cors');
+dotenv.config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        const db = client.db(process.env.AUTH_DB_NAME);
+        const jobCollection = db.collection('jobs');
+
+        app.post('/jobs', async (req, res) => {
+            const job = req.body;
+            const result = await jobCollection.insertOne(job);
+            res.send(result);
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Server is Serving...')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
