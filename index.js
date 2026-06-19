4721401e5ad3d573a36597244877465fc0bb847e
nusratjahan7 <nusratjahan77222@gmail.com>
2026-06-19 19:33:32 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -290,7 +290,7 @@ async function run() {
             res.send(updateResult);
         })
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
